17e64c60ded683a15f8aebcf22682faeb52284dc
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-04-29 15:09:51 -0700

    got the new create and show working

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -5,7 +5,24 @@ class CreaturesController < ApplicationController
   end
 
   def new
+    @creature = Creature.new
     render :new
+  end
+
+  def create
+    @creature = Creature.create(creature_params)
+    render :show
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+    render :show
+  end
+
+private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 
 end
