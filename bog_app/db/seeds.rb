ed6f656e1be607875de532bafbf534ffdf578cc0
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-04-30 11:17:34 -0700

    displayed on page

--- bog_app/db/seeds.rb
+++ bog_app/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Creature.destroy_all
+puts "DESTROYED"
+
+Creature.create({name: "Yoda", description: "Little green man"})
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
+puts "CREATEDDDDDDD"
