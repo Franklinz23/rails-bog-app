a63336a84e27189119ad75c0cf57a2d935217960
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-05-01 18:56:30 -0700

    2nd run done

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -14,8 +14,26 @@ class CreaturesController < ApplicationController
   end
 
   def show
-    @creature = Creature.find_by_id params[:id] 
+    @creature = Creature.find_by_id params[:id]
     render :show
+  end
+
+  def edit
+    @creature = Creature.find_by_id params[:id]
+    render :edit
+  end
+
+  def update
+    creature = Creature.find_by_id params[:id]
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature = Creature.find_by_id params[:id]
+    creature.destroy
+    redirect_to creatures_path
   end
 
 private
